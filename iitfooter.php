18a93610a08ed0073bf21aa5e16b1bb3258027ca
pchachoo <pchachoo@gmail.com>
2015-11-07 17:16:05 -0600

    Basic home page with nav bar with links to other pages, half slider and css for the site

---
+++ iitfooter.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+
+<footer>
+ <div id="footer-wrapper">
+    <h5 align="center"><img src="Image\iitlogo.jpg" alt="Illinois Institute of Technology"  height="72" align="absmiddle"/>
+    <a href="http://web.iit.edu/">IIT Main Campus, Chicago</a>  
+	</h5>
+</div>
+</footer>   
