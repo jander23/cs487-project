d342980f60c1d2efe08ed2acfdd2315654d69736
Jacob Freeman <Jacob@dhcp197.merunorth107.iit.edu>
2015-11-15 14:12:15 -0600

    Fix up areas script

--- tools/area.rb
+++ tools/area.rb
@@ -1,20 +1,20 @@
 puts "Full Name?"
-$stdout.flush #
+$stdout.flush
 name = gets.chomp
 puts "Short Name?"
-$stdout.flush #
+$stdout.flush
 short_name = gets.chomp
 puts "X1?"
-$stdout.flush #
+$stdout.flush
 X1 = gets.chomp
 puts "Y1?"
-$stdout.flush #
+$stdout.flush
 Y1 = gets.chomp
 puts "X2?"
-$stdout.flush #
+$stdout.flush
 X2 = gets.chomp
 puts "Y2?"
-$stdout.flush #
+$stdout.flush
 Y2 = gets.chomp
 
 area = "<area alt=\"#{name}\" title=\"#{name}\" href=\"building/#{short_name}.html\" data-toggle=\"tooltip\" data-placement=\"bottom\" shape=\"rect\" coords=\"#{X1},#{Y1},#{X2},#{Y2}\" style=\"outline:none;\" target=\"_self\">"
